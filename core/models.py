51ef1b0cc364fb7d54cb4e489773eb29259cac84
kalebe <kalebe613@gmail.com>
2023-07-01 12:20:46 +0100

    Create model.py

--- core/models.py
+++ core/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+import uuid
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFit
 
-# Create your models here.
+
+def getFilename(instance, filename):
+    ext = filename.split('.')[-1]
+    filename = f'{uuid.uuid4()}.{ext}'
+    return filename
+
+class AbstractBaseModel(models.Model):
+    name = models.CharField('Name', max_length=100)
+    created = models.DateTimeField('Created',auto_now_add=True)
+    updated = models.DateTimeField('Updated',auto_now=True)
+    activity = models.BooleanField('Activity', default=True)
+
+    class Meta: 
+        abstract = True
+
+class ProductModel(AbstractBaseModel):
+    price = models.DecimalField('Price', max_digits=9, decimal_places=2)
+    description = models.CharField('Description', max_length=300)
+    image = ProcessedImageField(
+        upload_to=getFilename,
+        processors=[ResizeToFit(width=300, height=300)],
+        max_length=1048,
+        options={'quality': 90},
+        blank=True,
+    )
+    class Meta:
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+    
+    def __str__(self):
+        return self.name
+    
