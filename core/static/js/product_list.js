7b526b15cbc0b29a7330cd7be844ed9402ed7294
kalebe <kalebe613@gmail.com>
2023-07-02 15:38:56 +0100

    Delete Product

--- core/static/js/product_list.js
+++ core/static/js/product_list.js
@@ -1,3 +1,4 @@
+// Start the product filter
 function filterProducts() {
     const searchInput = document.getElementById('search-input');
     const productList = document.getElementById('product-list');
@@ -17,3 +18,45 @@ function filterProducts() {
 
 const searchInput = document.getElementById('search-input');
 searchInput.addEventListener('keyup', filterProducts);
+// End the product filter
+
+//Start the code to delete the product
+
+    const modalTitle = document.querySelector('#deleteModal .modal-title');
+    const modalBody = document.querySelector('#deleteModal .modal-body');
+    const modal= document.getElementById("deleteModal");
+    modal.addEventListener('hidden.bs.modal',function(){
+        const form = modal.querySelector('form');
+        form.reset();
+        modalTitle.textContent =""
+        modalBody.innerHTML =""
+    });
+    
+
+    document.addEventListener('DOMContentLoaded', function() {
+    const deleteButtons = document.querySelectorAll('.delete-button');
+
+    deleteButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            
+            const productId = this.getAttribute('data-product-id');
+            const productName = this.getAttribute('data-product-name');
+            const productPrice = this.getAttribute('data-product-price');
+            
+            const modalTitle = document.querySelector('#deleteModal .modal-title');
+            const modalBody = document.querySelector('#deleteModal .modal-body');
+            
+            const deleteForm = document.getElementById('deleteForm');
+            const url = `/delete/${productId}/`;
+            deleteForm.setAttribute('action', url);
+
+            modalTitle.textContent += `Delete Product: ${productName}`;
+            modalBody.innerHTML += `
+                <p>Are you sure you want to delete the product?</p>
+                <p>Name: ${productName}</p>
+                <p>Price: ${productPrice}</p>
+            `;
+        });
+    });
+});
+
--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import ProductListView, ProductCreateView
+from .views import ProductListView, ProductCreateView, ProductDeleteView
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
    path('', ProductListView.as_view(), name='product_list'),
    path('create/', ProductCreateView.as_view(), name='product_create'),
+   path('delete/<int:pk>/', ProductDeleteView.as_view(), name='product_delete'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
